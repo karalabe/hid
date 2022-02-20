9177eb1b631492e27056f11b94d4e8f646bb933e
Olivier Mengué <dolmen@cpan.org>
2022-02-20 14:57:34 +0100

    Fix for incorrect reported written bytes on Windows
    Closes #36, #16

--- hid_enabled.go
+++ hid_enabled.go
@@ -164,8 +164,10 @@ func (dev *Device) Write(b []byte) (int, error) {
 	}
 	// Prepend a HID report ID on Windows, other OSes don't need it
 	var report []byte
+	fixlen := 0
 	if runtime.GOOS == "windows" {
 		report = append([]byte{0x00}, b...)
+		fixlen = -1
 	} else {
 		report = b
 	}
@@ -187,6 +189,9 @@ func (dev *Device) Write(b []byte) (int, error) {
 		}
 		failure, _ := wcharTToString(message)
 		return 0, errors.New("hidapi: " + failure)
+	}
+	if written > 0 {
+		written += fixlen
 	}
 	return written, nil
 }
